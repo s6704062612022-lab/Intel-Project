29160e676207ec4369f2116696091ef9ff912bf0
s6704062612022-lab <s6704062612022@email.kmutnb.ac.th>
2026-03-29 00:40:33 +0700

    Update 4_Product_Reviews.py

--- project/pages/4_Product_Reviews.py
+++ project/pages/4_Product_Reviews.py
@@ -1,15 +1,11 @@
 import streamlit as st
 import joblib
 from tensorflow.keras.models import load_model
-import os
 
 st.title("Product Review")
 
-model_path = os.path.join(os.path.dirname(__file__), "..", "reviews_model.h5")
-vectorizer_path = os.path.join(os.path.dirname(__file__), "..", "tfidf_vectorizer.pkl")
-
-model = load_model(model_path)
-vectorizer = joblib.load(vectorizer_path)
+model = load_model("reviews_model.h5")
+vectorizer = joblib.load("tfidf_vectorizer.pkl")
 
 text = st.text_area("Enter review")
 
